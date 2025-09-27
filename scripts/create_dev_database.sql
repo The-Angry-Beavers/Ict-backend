7287baecd821a60ae0587e30a6328717576579cc
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2025-09-27 16:05:55 +0300

    initial commit

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "mos-hack" SUPERUSER;
+CREATE DATABASE "mos-hack" OWNER "mos-hack" ENCODING 'utf-8';
